32213ac0d5bc579777b1beda0cd9499a465f0223
bhagwatcoder <bhagwatcoder@gmail.com>
2022-08-17 19:33:29 +0530

    Learning resources

---
+++ lib/screens/learning_resources/components/video_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class VideoItem extends StatelessWidget {
+  const VideoItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}

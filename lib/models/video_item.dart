3f92ced9985a3cd6bb2635608486da5851c21ad1
bhagwatcoder <bhagwatcoder@gmail.com>
2022-08-24 10:26:58 +0530

    Video player functionalty complete Issue #29

--- lib/models/video_item.dart
+++ lib/models/video_item.dart
@@ -3,12 +3,16 @@ class VideoMetaData{
   final String url;
   final String image;
   final String description;
+  final String language;
+  final String duration;
   VideoMetaData(
     {
       required this.description,
       required this.image,
       required this.name,
       required this.url,
+      required this.duration,
+      required this.language,
     }
   );
 }

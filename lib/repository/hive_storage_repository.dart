790b6c59b997bb7d739bf2b58fd04ac97bd3b9c5
bhagwatcoder <bhagwatcoder@gmail.com>
2022-09-03 17:23:58 +0530

    Secured local data using AES encryption

--- lib/repository/hive_storage_repository.dart
+++ lib/repository/hive_storage_repository.dart
@@ -1,18 +1,44 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:neoroo_app/models/baby_details_caregiver.dart';
 import 'package:neoroo_app/models/baby_details_family_member.dart';
 import 'package:neoroo_app/models/profile.dart';
 
 class HiveStorageRepository {
+  static final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+  static Future<List<int>> getKey() async {
+    if (await secureStorage.containsKey(key: "key")) {
+      return base64Url.decode((await secureStorage.read(key: 'key'))!);
+    }
+    List<int> key = Hive.generateSecureKey();
+    await secureStorage.write(
+      key: 'key',
+      value: base64Encode(key),
+    );
+    return key;
+  }
+
   static Future<bool> checkUserLoggedIn() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(key),
+    );
     print(box.containsKey("user"));
     return box.containsKey("user") && box.containsKey("selectedOrg");
   }
 
   Future<void> saveUserProfile(Profile profile) async {
+    List<int> key = await getKey();
     try {
-      Box box = await Hive.openBox("users");
+      Box box = await Hive.openBox(
+        "users",
+        encryptionCipher: HiveAesCipher(
+          key,
+        ),
+      );
       await box.put("user", profile);
     } catch (e) {
       print(e);
@@ -20,29 +46,59 @@ class HiveStorageRepository {
   }
 
   Future<Profile> getUserProfile() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     Profile profile = await box.get("user");
     return profile;
   }
 
   Future<void> saveOrganisationURL(String url) async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     await box.put("url", url);
   }
 
   Future<String> getOrganisationURL() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     return box.get("url");
   }
 
   Future<String?> getSelectedOrgName() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     return (await box.get("selectedOrgName"));
   }
 
   Future<void> saveCredentials(String username, String password,
       String serverURL, String? avatarId, String name) async {
-    Box box = await Hive.openBox("saved");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "saved",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     await box.put(
         username,
         avatarId == null
@@ -51,7 +107,13 @@ class HiveStorageRepository {
   }
 
   Future<Map<String, List<String>>> getSavedCredentials() async {
-    Box box = await Hive.openBox("saved");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "saved",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     print(box.keys);
     dynamic keys = box.keys.toList();
     Map<String, List<String>> data = {};
@@ -62,42 +124,90 @@ class HiveStorageRepository {
   }
 
   Future<void> saveOrganisations(List<String> organisationList) async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     await box.put("orgs", organisationList);
   }
 
   Future<List<String>> getSavedOrganisations() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     return (await box.get("orgs"));
   }
 
   Future<String> getSelectedOrganisation() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     return (await box.get("selectedOrg"));
   }
 
   Future<void> setIsCareGiver(bool isCareGiver) async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     await box.put("isCareGiver", isCareGiver);
   }
 
   Future<bool> getIsCareGiver() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     return (await box.get("isCareGiver"));
   }
 
   Future<void> setUserGroups(List<String> userGroups) async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     await box.put("userGroups", userGroups);
   }
 
   Future<List<String>> getUserGroups() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     return (await box.get("userGroups"));
   }
 
   Future<void> logOutUser() async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     box.clear();
     box = await Hive.openBox("baby");
     await box.clear();
@@ -105,21 +215,39 @@ class HiveStorageRepository {
   }
 
   Future<void> saveSelectedOrganisation(String id, String? name) async {
-    Box box = await Hive.openBox("users");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "users",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     await box.put("selectedOrg", id);
     await box.put("selectedOrgName", name);
   }
 
   Future<void> storeBabyFamilyMember(
       BabyDetailsFamilyMember babyDetailsFamilyMember) async {
-    Box box = await Hive.openBox("baby");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "baby",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     await box.put("baby", babyDetailsFamilyMember);
     return;
   }
 
   Future<BabyDetailsFamilyMember?> getBabyDetailsFamilyMember() async {
+    List<int> key = await getKey();
     if (await Hive.boxExists("baby")) {
-      Box box = await Hive.openBox("baby");
+      Box box = await Hive.openBox(
+        "baby",
+        encryptionCipher: HiveAesCipher(
+          key,
+        ),
+      );
       if (box.containsKey("baby")) {
         return box.get("baby");
       }
@@ -129,13 +257,25 @@ class HiveStorageRepository {
   }
 
   Future<List<BabyDetailsCaregiver>?> getBabyDetailsCaregiver() async {
+    List<int> key = await getKey();
     if (!await Hive.boxExists("babies")) {
       return null;
     }
-    if (!(await Hive.openBox("babies")).containsKey("list_of_babies")) {
+    if (!(await Hive.openBox(
+      "babies",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    ))
+        .containsKey("list_of_babies")) {
       return null;
     }
-    Box box = await Hive.openBox("babies");
+    Box box = await Hive.openBox(
+      "babies",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     List<BabyDetailsCaregiver> listOfBabies =
         List<BabyDetailsCaregiver>.from((await box.get("list_of_babies")));
     if (listOfBabies.isEmpty) {
@@ -146,7 +286,13 @@ class HiveStorageRepository {
 
   Future<void> saveBabyDetailsCaregiver(
       List<BabyDetailsCaregiver>? listOfBabies) async {
-    Box box = await Hive.openBox("babies");
+    List<int> key = await getKey();
+    Box box = await Hive.openBox(
+      "babies",
+      encryptionCipher: HiveAesCipher(
+        key,
+      ),
+    );
     try {
       await box.put("list_of_babies", listOfBabies);
     } catch (e) {
